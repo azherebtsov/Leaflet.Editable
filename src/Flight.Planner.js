eae859a9355e5bf276c9da67b45642abc8ea39ab
oz <zherebtsovalex@gmail.com>
2018-11-10 23:25:21 +0100

    Profile map added Waypoints have estimated altitude based on profile

--- src/Flight.Planner.js
+++ src/Flight.Planner.js
@@ -17,6 +17,9 @@
 
 }(function (L) {
 
+  const CRUISE_LEVEL = 340;
+  const LEVELS = [0, 50, 100, 180, 220, 260, 300, 340, 400];
+
   L.FlightPlanner = L.Evented.extend({
 
     onPolyLineClicked: function(layer) {
@@ -26,6 +29,59 @@
         window.localStorage.setItem('flight.planner.selected.waypoints', wpts);
       }
     },
+    onPathCreatedForProfile: function (layer) {
+      if( layer.getLatLngs() !== undefined && layer.getLatLngs().length > 1 ) {
+        var wpts = [];
+        var map = this;
+        var height = map.getContainer().clientHeight;
+        var width = map.getContainer().clientWidth;
+        var latlngs = layer.getLatLngs();
+        var ratio = height * latlngs.length / (width * CRUISE_LEVEL);
+
+        var distance = 0;
+        for (var i = 0; i < latlngs.length-1; i++) {
+          distance += latlngs[i].distanceTo(latlngs[i+1]);
+        }
+
+        latlngs.forEach(function (point, idx) {
+          // build profile chart
+          if(point.alt === undefined) {
+            if (idx !== 0 && idx < latlngs.length - 1) {
+              point.alt = CRUISE_LEVEL * ratio;
+            } else {
+              point.alt = 0;
+            }
+          }
+          var x = idx;
+          if( idx > 0 ) {
+            x = wpts[idx-1].lng + latlngs.length * latlngs[idx-1].distanceTo(latlngs[idx]) / distance;
+          }
+          var latlng = xy(x, point.alt);
+          wpts.push(latlng);
+        });
+        var profile = L.polyline(wpts).addTo(map);
+        profile.editing.enable();
+
+        LEVELS.forEach(function (level) {
+          // levels lines on profile map
+          addLevelBaseline(map, level, level * ratio);
+        });
+
+        this.setMaxBounds([
+          [-CRUISE_LEVEL * ratio / 10, -1],
+          [CRUISE_LEVEL * ratio * 2, wpts.length+2]
+        ]);
+        this.fitBounds(profile.getBounds());
+
+        profile.on('edit', function() {
+          // set altitude value for the points on route map
+          latlngs.forEach(function (point, idx) {
+              point.alt = wpts[idx].lat / ratio;
+            }
+          );
+        });
+      }
+    }
   });
 
   L.extend(L.FlightPlanner, {});
@@ -44,12 +100,36 @@
 
   });
 
+  var levelBaselines = [];
+
+  function addLevelBaseline(map, level, y) {
+    var baseline = L.polyline([[y, -1000], [y, 1000]],
+      {
+        color: '#BBAABB',
+        weight: 1
+      });
+    baseline.addTo(map);
+    var levelStr = "FL" + level;
+    baseline.setText(levelStr.padEnd(150),
+      {
+        repeat: true,
+        offset: -1,
+        attributes: {'font-weight': 'bold', 'font-size': '8', fill: "#BBAABB"}
+      });
+    levelBaselines.push(baseline);
+  }
+
   L.Map.addInitHook(function () {
 
     this.whenReady(function () {
       this.flightPlanner = new this.options.flightPlannerClass(this, this.options.flightPlannerOptions);
 
-      this.on('path:clicked', this.flightPlanner.onPolyLineClicked);
+      if( !this.options.profile ) {
+        this.on('path:clicked', this.flightPlanner.onPolyLineClicked);
+      } else {
+        // profile map
+        this.on('path:created', this.flightPlanner.onPathCreatedForProfile);
+      }
     });
 
   });
@@ -64,6 +144,14 @@
           Coordinates: {
             _latitude: Math.round(point.lat * 3600),
             _longitude: Math.round(point.lng * 3600)
+          },
+          Altitude: {
+            EstimatedAltitude: {
+              Value: {
+                _unit: "ft/100",
+                __text: Math.round(point.alt)
+              }
+            }
           }
         };
         points.push(waypoint);
@@ -77,5 +165,14 @@
     };
     return x2js.json2xml_str(root)
   }
+
+  var yx = L.latLng;
+
+  var xy = function(x, y) {
+    if (L.Util.isArray(x)) {    // When doing xy([x, y]);
+      return yx(x[1], x[0]);
+    }
+    return yx(y, x);  // When doing xy(x, y);
+  };
 
 }, window));
